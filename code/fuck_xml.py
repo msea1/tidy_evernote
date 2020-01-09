6bfe0de9e7ef4eb1a1fcbd9723c58f5615fee201
Matthew Carruth <mcarruth@blacksky.com>
2020-01-08 18:43:18 -0800

    better ordering

--- code/fuck_xml.py
+++ code/fuck_xml.py
@@ -3,12 +3,12 @@ import re
 BREAK_BETWEEN_NOTES = "<hr />"
 
 
-with open('../evernote_export/notes.enex') as f:
+with open('../import_evernote/2015_b.enex') as f:
     contents = f.read()
 
 notes = re.findall(r"<note>(.*?)</note>", contents, re.MULTILINE | re.DOTALL)
 
-ordered_by_title = {}
+ordered_by = {}
 all_tags = set()
 created_time = None
 last_updated = None
@@ -16,8 +16,6 @@ last_updated = None
 for note in notes:
     title = re.search(r"<title>(.*?)</title>", note, re.MULTILINE | re.DOTALL).group(1)
     contents = re.search(r"<en-note.*?>(.*?)</en-note>", note, re.MULTILINE | re.DOTALL).group(1)
-    ordered_by_title[title] = f'<div><font style="font-size: 18px;">{title}</font></div>' \
-                              f'\n{contents}\n\n\n'
 
     created = re.search(r"<created>(.*?)</created>", note, re.MULTILINE | re.DOTALL).group(1)
     if created_time is None or created_time > created:
@@ -30,8 +28,11 @@ for note in notes:
     tags = re.findall(r"<tag>(.*?)</tag>", note, re.MULTILINE | re.DOTALL)
     all_tags.update(set(tags))
 
+    ordered_by[f'{created}_{title}'] = f'<div><font style="font-size: 18px;">{title}</font></div>' \
+                        f'\n{contents}\n\n\n'
 
-title = 'blitz'
+
+title = '2015b_Journal'
 # allow custom title of file and also title of note
 with open(f'../import_evernote/{title}.enex', 'w') as f:
     f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
@@ -41,8 +42,8 @@ with open(f'../import_evernote/{title}.enex', 'w') as f:
     f.write(f'<content>\n')
     f.write('<![CDATA[<!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd"><en-note>\n')
 
-    for o in sorted(ordered_by_title):
-        f.write(ordered_by_title[o])
+    for o in sorted(ordered_by):
+        f.write(ordered_by[o])
         f.write(BREAK_BETWEEN_NOTES)
 
     f.write('\n</en-note]]>\n</content>\n')
